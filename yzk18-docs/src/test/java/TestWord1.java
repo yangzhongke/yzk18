0359e2048ce57292fdecb27b649788253b7676d5
yzk <yzk365@qq.com>
2021-04-18 18:07:11 +0800

    add WordChartBuilder

--- yzk18-docs/src/test/java/TestWord1.java
+++ yzk18-docs/src/test/java/TestWord1.java
@@ -1,11 +1,19 @@
 import com.yzk18.commons.DesktopHelpers;
 import com.yzk18.commons.IOHelpers;
+import com.yzk18.docs.WordChartBuilder;
 import com.yzk18.docs.WordHelpers;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.Units;
+import org.apache.poi.xddf.usermodel.chart.*;
 import org.apache.poi.xwpf.usermodel.*;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class TestWord1 {
+
+
     public static void main(String[] args) {
         /*
         String s = WordHelpers.readAllText("F:\\网友提供的阅读\\零散资料\\高考阅读高频单词.doc");
@@ -63,7 +71,23 @@ public class TestWord1 {
         WordHelpers.setTableCellValue(table1,1,2, LocalDate.of(1981,6,6));
         WordHelpers.setTableCellValue(table1,1,3,IOHelpers.readAllBytes("E:\\temp\\small.jpg"));
 
+        String[] monthNames = {"一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"};
+        Double[] sales1 = {3.1,5.2,8.0,9.0,3.0,5.0,8.0,9.0,3.0,5.0,8.0,0.9};
+        Double[] sales2={5.0,81.0,4.0,9.8,1.8,3.9,5.8,81.8,4.8,9.8,1.8,3.9};
 
+        WordChartBuilder<Double> chartBuilder = new WordChartBuilder<>(ChartTypes.LINE);
+        XWPFChart chart = chartBuilder
+                .putValues("小王",sales1)
+                .putValues("小李",sales2)
+                .setCategoryAxisTitle("月份").setValueAxisTitle("销量")
+               .setSize(500,300)
+                .setCategoryNames(monthNames)
+            .build(doc);
+        chart.setTitleText("销售龙虎榜");
+        chart.getOrAddLegend().setPosition(LegendPosition.LEFT);
+        /*
+        chart.setChartHeight(Units.toEMU(100));
+        chart.setChartWidth(Units.toEMU(300));*/
 
 
         //todo: chart. doc.createChart().createData()
