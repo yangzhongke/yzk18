1aad9e64621a3b799262bc5447eb6b3a43649ede
yzk <yzk365@qq.com>
2021-04-23 21:42:41 +0800

    pom.xml of yzk18-commons done

--- tests/src/main/java/批量生成学生卡/Main.java
+++ tests/src/main/java/批量生成学生卡/Main.java
@@ -61,5 +61,29 @@ public class Main {
         }
         return null;
     }
+
+    public BufferedImage scaleImage(BufferedImage img, int width, int height,
+                                    Color background) {
+        int imgWidth = img.getWidth();
+        int imgHeight = img.getHeight();
+        if (imgWidth*height < imgHeight*width) {
+            width = imgWidth*height/imgHeight;
+        } else {
+            height = imgHeight*width/imgWidth;
+        }
+        BufferedImage newImage = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = newImage.createGraphics();
+        try {
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                    RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            g.setBackground(background);
+            g.clearRect(0, 0, width, height);
+            g.drawImage(img, 0, 0, width, height, null);
+        } finally {
+            g.dispose();
+        }
+        return newImage;
+    }
 }
 
